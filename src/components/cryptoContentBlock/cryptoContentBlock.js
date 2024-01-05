7b112bc0165e84a2bb2808f01cdaa472ecdd7953
Artem <direktonn@gmail.com>
2024-01-05 15:57:23 +0200

    added function for help to search

--- src/components/cryptoContentBlock/cryptoContentBlock.js
+++ src/components/cryptoContentBlock/cryptoContentBlock.js
@@ -1,20 +1,19 @@
-import React from "react";
-import { useState, useEffect } from "react";
-
+import React, { useState, useEffect } from "react";
 import CryptoInfo from "../cryptoInfo/CryptoInfo";
 import CustomizedTables from "../table/Table";
+import SearchBar from "../searchBar/searchBar";
 
 const CryptoContentBlock = () => {
   const [cryptoData, setCryptoData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch("https://api.coincap.io/v2/assets");
-        console.log(response);
-
         const data = await response.json();
-        setCryptoData(data);
+        setCryptoData(data.data);
+        setFilteredData(data.data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -22,14 +21,23 @@ const CryptoContentBlock = () => {
 
     fetchData();
   }, []);
-  console.log(cryptoData);
-  if (cryptoData === 0) {
+
+  const handleSearch = (searchValue) => {
+    const filteredResults = cryptoData.filter((item) =>
+      item.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setFilteredData(filteredResults);
+  };
+
+  if (cryptoData.length === 0) {
     return null;
   }
+
   return (
     <>
+      <SearchBar onSearch={handleSearch} />
       <CryptoInfo data={cryptoData.data} />
-      <CustomizedTables data={cryptoData.data} />
+      <CustomizedTables data={filteredData} />
     </>
   );
 };
