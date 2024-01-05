6c86f1315172dcb2d07b5acb7b0a2f8b6edb4482
Artem <direktonn@gmail.com>
2024-01-05 15:58:36 +0200

    updated

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,14 +1,12 @@
 import React from "react";
 
 import AppHeader from "../appheader/AppHeader";
-import SearchBar from "../searchBar/searchBar";
 import CryptoContentBlock from "../cryptoContentBlock/cryptoContentBlock";
 
 const HomePage = () => {
   return (
     <div>
       <AppHeader />
-      <SearchBar />
       <CryptoContentBlock />
     </div>
   );
