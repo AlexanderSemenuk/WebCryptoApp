4d4d424af4a2f5eff1a7f518734159bbb3189ce9
Artem <direktonn@gmail.com>
2024-01-17 16:38:24 +0200

    added routes

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import AppHeader from "../appheader/AppHeader";
 import CryptoContentBlock from "../cryptoContentBlock/cryptoContentBlock";
+import Footer from "../footer/Footer";
+import CryptoDetailPage from "../cryptoDetailPage/CryptoDetailPage";
 
 const HomePage = () => {
   return (
-    <div>
+    <Router>
       <AppHeader />
-      <CryptoContentBlock />
-    </div>
+      <Routes>
+        <Route exact path="/" element={<CryptoContentBlock />} />
+        <Route path="/crypto/:cryptoId" element={<CryptoDetailPage />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 };
 
