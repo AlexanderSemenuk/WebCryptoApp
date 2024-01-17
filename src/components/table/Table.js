5bfc209a87c517f49bbed8060332f161d5482f75
Artem <direktonn@gmail.com>
2024-01-17 16:39:24 +0200

    updated

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,5 +1,4 @@
-import * as React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { styled } from "@mui/material/styles";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -8,6 +7,12 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { useNavigate } from "react-router-dom";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import SouthEastIcon from "@mui/icons-material/SouthEast";
+import NorthEastIcon from "@mui/icons-material/NorthEast";
+import { formatPercentage, formatNumber } from "./../function/function.js";
 
 import "./Table.scss";
 
@@ -17,69 +22,78 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     backgroundColor: "rgb(118, 118, 233)",
   },
   [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
+    fontSize: 16,
+  },
+  "&.custom-hover": {
+    cursor: "pointer",
+    "&:hover": {
+      backgroundColor: "rgb(93, 93, 224)",
+      transition: "0.5s all",
+    },
   },
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
+    "&:hover": {
+      backgroundColor: theme.palette.grey[300],
+      transition: "0.3s",
+    },
   },
   // hide last border
   "&:last-child td, &:last-child th": {
     border: 0,
   },
+  cursor: "pointer",
+  "&:hover": {
+    backgroundColor: theme.palette.grey[300],
+    transition: "0.3s",
+  },
 }));
 
-export default function CustomizedTables({ data }) {
+const CustomizedTables = ({ data }) => {
   const [displayCount, setDisplayCount] = useState(20);
+  const [sortOrder, setSortOrder] = useState({
+    field: null,
+    ascending: true,
+  });
+  const [sortDirection, setSortDirection] = useState("asc");
 
-  const formatNumber = (number) => {
-    // Округляем число до двух знаков после запятой
-    const roundedNumber = parseFloat(number).toFixed(2);
+  const navigate = useNavigate();
 
-    // Разделяем число на целую и десятичную части
-    let [integerPart, decimalPart] = roundedNumber.split(".");
+  const dataArray = Object.values(data);
 
-    // Добавляем разделитель для тысяч в целой части
-    integerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-
-    // Собираем отформатированное число
-    const formattedNumber = decimalPart
-      ? `${integerPart}.${decimalPart}`
-      : integerPart;
-
-    // Определяем суффиксы для тысяч, миллионов, миллиардов и т.д.
-    const suffixes = ["", "K", "M", "B", "T"];
-
-    let suffixIndex = 0;
-    let formattedNumberWithSuffix = parseFloat(number);
-
-    while (
-      formattedNumberWithSuffix >= 1000 &&
-      suffixIndex < suffixes.length - 1
-    ) {
-      formattedNumberWithSuffix /= 1000;
-      suffixIndex++;
-    }
-
-    // Округляем число до двух знаков после запятой
-    formattedNumberWithSuffix = parseFloat(
-      formattedNumberWithSuffix.toFixed(2)
-    );
-
-    // Добавляем суффикс
-    const numberWithSuffix =
-      formattedNumberWithSuffix.toString() + suffixes[suffixIndex];
-
-    return { formattedNumber, numberWithSuffix };
-  };
-
-  const limitedData = data?.slice(0, displayCount);
+  const limitedData = Array.isArray(dataArray)
+    ? dataArray.slice(0, displayCount)
+    : [];
 
   const loadMore = () => {
     setDisplayCount((prevDisplayCount) => prevDisplayCount + 10);
   };
+
+  const handleSort = (field) => {
+    setSortOrder((prevSortOrder) => ({
+      field,
+      ascending:
+        prevSortOrder.field === field ? !prevSortOrder.ascending : true,
+    }));
+
+    setSortDirection((prevSortDirection) =>
+      prevSortDirection === "asc" ? "desc" : "asc"
+    );
+  };
+
+  const sortedData = [...limitedData].sort((a, b) => {
+    const fieldA = a[sortOrder.field];
+    const fieldB = b[sortOrder.field];
+
+    if (sortOrder.ascending) {
+      return fieldA - fieldB;
+    } else {
+      return fieldB - fieldA;
+    }
+  });
 
   return (
     <div className="Table">
@@ -93,28 +107,112 @@ export default function CustomizedTables({ data }) {
         <Table sx={{ minWidth: 700 }} aria-label="customized table">
           <TableHead>
             <TableRow>
-              <StyledTableCell>Name</StyledTableCell>
-              <StyledTableCell align="right">Price</StyledTableCell>
+              <StyledTableCell
+                onClick={() => handleSort("name")}
+                className="custom-hover"
+              >
+                Name{" "}
+                {sortOrder.field === "name" && (
+                  <>
+                    {sortDirection === "asc" ? (
+                      <ArrowDropDownIcon />
+                    ) : (
+                      <ArrowDropUpIcon />
+                    )}
+                  </>
+                )}
+              </StyledTableCell>
+              <StyledTableCell
+                onClick={() => handleSort("priceUsd")}
+                align="right"
+                className="custom-hover"
+              >
+                Price{" "}
+                {sortOrder.field === "priceUsd" && (
+                  <>
+                    {sortDirection === "asc" ? (
+                      <ArrowDropDownIcon />
+                    ) : (
+                      <ArrowDropUpIcon />
+                    )}
+                  </>
+                )}
+              </StyledTableCell>
               <StyledTableCell align="right">Chart</StyledTableCell>
-              <StyledTableCell align="right">Change</StyledTableCell>
-              <StyledTableCell align="right">Market Cap</StyledTableCell>
+              <StyledTableCell
+                onClick={() => handleSort("changePercent24Hr")}
+                align="right"
+                className="custom-hover"
+              >
+                Change{" "}
+                {sortOrder.field === "changePercent24Hr" && (
+                  <>
+                    {sortDirection === "asc" ? (
+                      <ArrowDropDownIcon />
+                    ) : (
+                      <ArrowDropUpIcon />
+                    )}
+                  </>
+                )}
+              </StyledTableCell>
+              <StyledTableCell
+                onClick={() => handleSort("marketCapUsd")}
+                align="right"
+                className="custom-hover"
+              >
+                Market Cap{" "}
+                {sortOrder.field === "marketCapUsd" && (
+                  <>
+                    {sortDirection === "asc" ? (
+                      <ArrowDropDownIcon />
+                    ) : (
+                      <ArrowDropUpIcon />
+                    )}
+                  </>
+                )}
+              </StyledTableCell>
               <StyledTableCell align="right">Volume (24h)</StyledTableCell>
               <StyledTableCell align="right">Supply</StyledTableCell>
-              <StyledTableCell align="right">Trade</StyledTableCell>
+              <StyledTableCell align="center">Trade</StyledTableCell>
             </TableRow>
           </TableHead>
+
           <TableBody>
-            {limitedData?.map((item) => (
-              <StyledTableRow key={item.id}>
+            {sortedData?.map((item) => (
+              <StyledTableRow
+                key={item.id}
+                onClick={() => {
+                  navigate(`/crypto/${item.id}`);
+                }}
+              >
                 <StyledTableCell component="th" scope="row">
-                  <img src={item.imageUrl} alt="thumbnail" />
-                  {item.name}
+                  <div className="Table__name">
+                    <img
+                      src={item.imageUrl}
+                      alt="thumbnail"
+                      className="Table__thumbnail"
+                    />
+                    <span>{item.name}</span>
+                  </div>
                 </StyledTableCell>
                 <StyledTableCell align="right">
                   ${formatNumber(item.priceUsd).formattedNumber}
                 </StyledTableCell>
-                <StyledTableCell align="right">{item.Chart}</StyledTableCell>
-                <StyledTableCell align="right">{item.Change}</StyledTableCell>
+                <StyledTableCell align="right">{item.chart}</StyledTableCell>
+                <StyledTableCell align="right">
+                  <span
+                    className={
+                      item.changePercent24Hr < 0 ? "negative" : "positive"
+                    }
+                  >
+                    {item.changePercent24Hr < 0 ? (
+                      <SouthEastIcon />
+                    ) : (
+                      <NorthEastIcon />
+                    )}
+                    {formatPercentage(item.changePercent24Hr)}
+                  </span>
+                </StyledTableCell>
                 <StyledTableCell align="right">
                   ${formatNumber(item.marketCapUsd).numberWithSuffix}
                 </StyledTableCell>
@@ -124,7 +222,7 @@ export default function CustomizedTables({ data }) {
                 <StyledTableCell align="right">
                   {formatNumber(item.supply).numberWithSuffix}
                 </StyledTableCell>
-                <StyledTableCell align="right">
+                <StyledTableCell align="center">
                   <button className="Table__button">Trade</button>
                 </StyledTableCell>
               </StyledTableRow>
@@ -132,11 +230,12 @@ export default function CustomizedTables({ data }) {
           </TableBody>
         </Table>
       </TableContainer>
-      {data.length > displayCount && (
+      {dataArray.length > displayCount && (
         <button onClick={loadMore} className="Table__load">
           Load More
         </button>
       )}
     </div>
   );
-}
+};
+export default CustomizedTables;
