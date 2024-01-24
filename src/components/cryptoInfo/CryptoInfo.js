16b103288958d314504acc5042d9f45d92904313
Artem <direktonn@gmail.com>
2024-01-25 01:12:21 +0200

    updated

--- src/components/cryptoInfo/CryptoInfo.js
+++ src/components/cryptoInfo/CryptoInfo.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { formatPercentage, formatedNumber } from "./../function/function.js";
+import NorthEastIcon from "@mui/icons-material/NorthEast";
 
 import "./cryptoInfo.scss";
 
@@ -22,6 +23,12 @@ const CryptoInfo = () => {
     };
 
     fetchData();
+
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 10000);
+
+    return () => clearInterval(intervalId);
   }, []);
 
   if (!cryptoData || cryptoData.length === 0) {
@@ -42,7 +49,14 @@ const CryptoInfo = () => {
             navigate(`/crypto/${item.id}`);
           }}
         >
-          <img src={item.imageUrl} alt="thumbnail" />
+          <img
+            src={item.imageUrl}
+            alt="thumbnail"
+            onError={(e) => {
+              e.target.src =
+                "https://cdn.discordapp.com/attachments/855187055940075530/1161619083045916692/logo.png?ex=65c36293&is=65b0ed93&hm=cb690e6ef95f1a36aace54556c6aa6cb97082a4875cb79d536c0003366fa256f&";
+            }}
+          />
           <div className="Crypto__info">
             <div className="Crypto__name">
               <span>
@@ -52,7 +66,10 @@ const CryptoInfo = () => {
             </div>
             <div className="Crypto__price">
               <span>${formatedNumber(item.priceUsd)}</span>
-              <span>{formatPercentage(item.changePercent24Hr)}</span>
+              <span className="positive">
+                <NorthEastIcon />
+                {formatPercentage(item.changePercent24Hr)}
+              </span>
             </div>
           </div>
         </li>
